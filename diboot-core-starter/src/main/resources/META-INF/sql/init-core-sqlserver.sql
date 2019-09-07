1418ac3dc677ad89f5be01e8a0a93d6f282653c7
mazhicheng <new123456>
2019-09-07 15:45:19 +0800

    SQL Server备注中文乱码

--- diboot-core-starter/src/main/resources/META-INF/sql/init-core-sqlserver.sql
+++ diboot-core-starter/src/main/resources/META-INF/sql/init-core-sqlserver.sql
@@ -15,19 +15,19 @@ create table ${SCHEMA}.dictionary (
    constraint PK_dictionary primary key (id)
 );
 -- 添加备注
-execute sp_addextendedproperty 'MS_Description', 'ID', 'SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'id';
-execute sp_addextendedproperty 'MS_Description', '父ID','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'parent_id';
-execute sp_addextendedproperty 'MS_Description', '字典类型','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'type';
-execute sp_addextendedproperty 'MS_Description', '显示名','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'item_name';
-execute sp_addextendedproperty 'MS_Description', '存储值','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'item_value';
-execute sp_addextendedproperty 'MS_Description', '备注','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'comment';
-execute sp_addextendedproperty 'MS_Description', '扩展JSON','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'extdata';
-execute sp_addextendedproperty 'MS_Description', '排序号','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'sort_id';
-execute sp_addextendedproperty 'MS_Description', '是否系统预置','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'system';
-execute sp_addextendedproperty 'MS_Description', '是否可编辑','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'editable';
-execute sp_addextendedproperty 'MS_Description', '删除标记','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'deleted';
-execute sp_addextendedproperty 'MS_Description', '创建时间','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'create_time';
+execute sp_addextendedproperty 'MS_Description', N'ID', 'SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'id';
+execute sp_addextendedproperty 'MS_Description', N'父ID','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'parent_id';
+execute sp_addextendedproperty 'MS_Description', N'字典类型','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'type';
+execute sp_addextendedproperty 'MS_Description', N'显示名','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'item_name';
+execute sp_addextendedproperty 'MS_Description', N'存储值','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'item_value';
+execute sp_addextendedproperty 'MS_Description', N'备注','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'comment';
+execute sp_addextendedproperty 'MS_Description', N'扩展JSON','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'extdata';
+execute sp_addextendedproperty 'MS_Description', N'排序号','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'sort_id';
+execute sp_addextendedproperty 'MS_Description', N'是否系统预置','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'system';
+execute sp_addextendedproperty 'MS_Description', N'是否可编辑','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'editable';
+execute sp_addextendedproperty 'MS_Description', N'删除标记','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'deleted';
+execute sp_addextendedproperty 'MS_Description', N'创建时间','SCHEMA', '${SCHEMA}', 'table', dictionary, 'column', 'create_time';
 
-execute sp_addextendedproperty 'MS_Description', '数据字典','SCHEMA', '${SCHEMA}', 'table', dictionary, null, null;
+execute sp_addextendedproperty 'MS_Description', N'数据字典','SCHEMA', '${SCHEMA}', 'table', dictionary, null, null;
 -- 创建索引
 create nonclustered index idx_directory on ${SCHEMA}.dictionary(type, item_value);
