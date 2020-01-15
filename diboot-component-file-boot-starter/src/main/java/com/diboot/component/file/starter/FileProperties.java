2fde331a7047f8f557122cf81c8d9bd91d6ea108
dengxiaojie <452796028@qq.com>
2020-01-15 10:30:34 +0800

    need author

--- diboot-component-file-boot-starter/src/main/java/com/diboot/component/file/starter/FileProperties.java
+++ diboot-component-file-boot-starter/src/main/java/com/diboot/component/file/starter/FileProperties.java
@@ -3,6 +3,9 @@ package com.diboot.component.file.starter;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * @author diboot
+ */
 @Data
 @ConfigurationProperties(prefix = "diboot.component.file")
 public class FileProperties {
